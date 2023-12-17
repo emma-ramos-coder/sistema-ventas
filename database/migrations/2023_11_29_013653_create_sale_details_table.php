f109fa0e1bc7de29f00fbc1a2f8e1ab5545ac60e
Emma Ramos <emmyramosc@gmail.com>
2023-12-16 19:56:14 -0500

    corregir migracion

--- database/migrations/2023_11_29_013653_create_sale_details_table.php
+++ database/migrations/2023_11_29_013653_create_sale_details_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
                 ->on('articles')
                 ->onDelete('cascade');
             $table->decimal('quantity',10,0);
-            $table->decimal('price',10,0);
+            $table->decimal('price',10,2);
             $table->timestamps();
         });
     }
