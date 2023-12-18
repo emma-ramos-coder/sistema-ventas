b82d280055d1c0ba9d4b05e0e059da717ec94a93
Emma Ramos <emmyramosc@gmail.com>
2023-12-17 21:38:03 -0500

    script

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,12 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
     use HasRoles;
 
+    static $rules = [
+		'name' => 'required',
+		'email' => 'required|email',
+        'password' => 'required!min:8',
+    ];
+
     /**
      * The attributes that are mass assignable.
      *
